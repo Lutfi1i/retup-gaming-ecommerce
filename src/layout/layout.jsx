85a05d0d1deb184ed8761c2ba2ed771024f1e5a5
Naufal Lutfi <kazuyaicikawa@gmail.com>
2025-06-06 16:37:10 +0700

    Update layout.jsx
    checkout no navbar & footer

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -4,8 +4,10 @@ import { Outlet, useLocation } from "react-router-dom";
 
 function Layout () {
     const location = useLocation();
-    const HideNavbar = ['/login', '/register'];
-    const CheckisNavbarHidden = HideNavbar.includes(location.pathname);
+    const HideNavbar = ['/login', '/register', '/checkout'];
+    const HideFooter = ['/checkout'];
+    const checkisFooterHidden = HideBoth.includes(location.pathname);
+    const checkisNavbarHidden = HideBoth.includes(location.pathname);
 
     return (
         <>
@@ -13,7 +15,7 @@ function Layout () {
         <main>
             <Outlet />
         </main>
-        <Footer />
+        {!CheckisCheckisFooterHidden && <Footer />}
         </>
     )
 
